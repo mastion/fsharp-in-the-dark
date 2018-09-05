33ad2fd8f9882e767cd319b4f6c86d1d7444713c
Peter Finn <peter.james.finn.1@gmail.com>
2018-09-04 18:08:44 -0700

    encapsulated an action roll type

--- blades.core/Types.fs
+++ blades.core/Types.fs
@@ -1,5 +1,6 @@
 namespace Blades.Core
 
+open System.Runtime.CompilerServices
 [<RequireQualifiedAccess>]
 module Types =
     type ActionSuccesses = 
@@ -13,3 +14,33 @@ module Types =
             success : ActionSuccesses;
             failures : int
         }
+
+    type Position =
+        | Controlled
+        | Risky
+        | Desperate
+
+    type Effect =
+        | Limited
+        | Standard
+        | Great   
+
+    type Attribute =
+        | Attune
+        | Command
+        | Consort
+        | Finesse
+        | Hunt
+        | Prowl
+        | Skirmish
+        | Study
+        | Survey
+        | Sway
+        | Tinker
+        | Wreck
+
+    type ActionRoll = {
+        position : Position;
+        attributeRank : int; //todo put this in a type
+        effect: Effect; 
+    }    
