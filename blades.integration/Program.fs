9dbc1995ce32ed462e297b450506858228653585
Peter Finn <peter.james.finn.1@gmail.com>
2018-10-31 22:24:19 -0700

    adding integration - very slow

---
+++ blades.integration/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Blades.Core
+
+[<EntryPoint>]
+let main argv =
+    let rollInfo : Types.ActionRoll = {
+        position = Types.Position.Controlled
+        attributeRank = 7
+        effect = Types.Effect.Standard
+    }
+    printfn "Rolling %i with position %O and effect %O" rollInfo.attributeRank rollInfo.position rollInfo.effect
+    let actionResult = Scoundrel.actionRoll rollInfo
+    printfn "Roll result %O and with %i failures" actionResult.success actionResult.failures
+    0 // return an integer exit code
