01d36b8789cea9e423748e4da2f5286202c10381
Peter Finn <peter.james.finn.1@gmail.com>
2018-10-31 22:26:59 -0700

    adding stopwatch
    roll taking way to long and never seems to get a full or crit

--- blades.integration/Program.fs
+++ blades.integration/Program.fs
@@ -7,10 +7,13 @@ open Blades.Core
 let main argv =
     let rollInfo : Types.ActionRoll = {
         position = Types.Position.Controlled
-        attributeRank = 7
+        attributeRank = 12
         effect = Types.Effect.Standard
     }
     printfn "Rolling %i with position %O and effect %O" rollInfo.attributeRank rollInfo.position rollInfo.effect
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
     let actionResult = Scoundrel.actionRoll rollInfo
+    stopWatch.Stop()
     printfn "Roll result %O and with %i failures" actionResult.success actionResult.failures
+    printfn "Roll took: %f" stopWatch.Elapsed.TotalMilliseconds
     0 // return an integer exit code
